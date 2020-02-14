467d2b52eede6df5dd5e69c9f87e7fa9ba5de938
AlexDelitzas <alexanderdel9@gmail.com>
2020-02-14 22:08:31 +0200

    Fix helper bash script

--- utils/send_files_to_devices.sh
+++ utils/send_files_to_devices.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sshpass -p 'espx2019' scp ../main_8450 root@10.0.84.50:/root/main
+sshpass -p 'espx2019' scp ../bin/main_8450 root@10.0.84.50:/root/bin/main
 
-sshpass -p 'espx2019' scp ../main_8448 root@10.0.84.48:/root/main
+sshpass -p 'espx2019' scp ../bin/main_8448 root@10.0.84.48:/root/bin/main
